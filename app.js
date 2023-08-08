e0bf8df2f685a66f6d9f8deb7cdb2ba5d4157084
Shriteen <86562935+Shriteen@users.noreply.github.com>
2023-08-08 19:21:18 +0530

    Improvements to work with static (public) files
    - Make public directory serve static at /public path *also*
      Previously contents of public were only available at / which meant we couldn't access them as /public/
    
    - add public/images to gitignore so its not affected by uploads

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// things inside public directory can be accessed both as if at server root or with explicit prefix 'public/'
+app.use('/public',express.static(path.join(__dirname, 'public')));
 
 app.use(session({
     secret: 'secret',
