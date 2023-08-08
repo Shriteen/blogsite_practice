77945e6d26f45f46855343779e2eb2480240d855
Shriteen <86562935+Shriteen@users.noreply.github.com>
2023-08-08 19:48:41 +0530

    Dashboard, create and edit post
    - Dashboard (separate controller, route is included in admin)
    - Create and edit post form with handlers
    
    Note: Dashboard view is temporary

---
+++ controllers/dashboardController.js
@@ -0,0 +1,169 @@
+const asyncHandler = require('express-async-handler');
+const {body, validationResult}= require('express-validator');
+
+const Author = require('../models/authorModel.js');
+const Admin = require('../models/adminModel.js');
+const Post = require('../models/postModel.js');
+
+exports.dashboard= asyncHandler(async function(req,res){
+    res.render('dashboard');
+});
+
+exports.createPost= asyncHandler(async function(req,res){
+    const allAuthors= await Author.find().exec();
+    const admin=await Admin.findById(req.session.userid).exec();
+    res.render('post_form',{
+	title: 'Create Post',
+	allAuthors: allAuthors,
+	author: admin.username
+    });
+});
+
+exports.createPostHandle= [
+    body("title","Title must be provided")
+	.trim()
+	.isLength({min: 1})
+	.escape(),
+    body("author","Author must be provided")
+	.trim()
+	.isLength({min: 1})
+	.escape(),
+    //assuming editorjs data is available and valid
+    asyncHandler(async function(req,res){
+	const errors= validationResult(req);
+
+	let author;
+	if(req.body.author)
+	    //express-validator just lists errors, we handle the error
+	    //later.. The code in function runs regardless of tests
+	    //have passed or failed. Hence existance of non-empty string is checked
+	{
+	    author= await Author.findOneAndUpdate(
+		{name: req.body.author},
+		{},
+		{
+		    new: true,
+		    upsert: true
+		});
+	}
+
+	// Note that below does not need any checking as we don't save
+	// until we check for errors. Above code was also saving into
+	// the DB
+	const post= await new Post({
+	    title: req.body.title,
+	    author: author._id,
+	    content: req.body.editorjs,
+	    editorAdmins: [ req.session.userid ], // When creating only one admin is relevant
+	    comments: []
+	}).populate("author");
+
+	if(!errors.isEmpty())
+	{
+	    const allAuthors= await Author.find().exec();
+	    res.render('post_form',{
+		title: 'Create Post',
+		allAuthors,
+		post,
+		oldData: req.body.editorjs,
+		errors: errors.array()
+	    });
+	}
+	else
+	{
+	    await post.save();
+	    res.redirect(post.url);
+	}
+    })
+];
+
+exports.editPost= asyncHandler(async function(req,res){
+    
+    const [post,allAuthors]= await Promise.all([
+	Post.findById(req.params.postId).populate('author'),
+	Author.find().exec()
+    ]);
+
+    if(post) {
+	res.render('post_form',{
+	    title: 'Edit Post: '+ post.title ,
+	    allAuthors,
+	    post,
+	    oldData: post.content
+	});
+    }
+    else
+	res.sendStatus(404);    
+    
+});
+
+exports.editPostHandle= [
+    body("title","Title must be provided")
+	.trim()
+	.isLength({min: 1})
+	.escape(),
+    body("author","Author must be provided")
+	.trim()
+	.isLength({min: 1})
+	.escape(),
+    //assuming editorjs data is available and valid
+    asyncHandler(async function(req,res){
+	const errors= validationResult(req);
+
+	let author;
+	if(req.body.author)
+	    //express-validator just lists errors, we handle the error
+	    //later.. The code in function runs regardless of tests
+	    //have passed or failed. Hence existance of non-empty string is checked
+	{
+	    author= await Author.findOneAndUpdate(
+		{name: req.body.author},
+		{},
+		{
+		    new: true,
+		    upsert: true
+		});
+	}
+	
+	const post= (await Post.findById(req.params.postId).exec())?.populate("author");
+	
+	if(post)
+	    // edit post form shouldn't allow invalid Id to be
+	    // submitted, but manually crafted attack is possible
+	{
+	    if(!errors.isEmpty())
+	    {
+		post.title=req.body.title;
+		post.author=author._id;
+		post.content=req.body.editorjs;
+		
+		const allAuthors= await Author.find().exec();
+		res.render('post_form',{
+		    title: 'Edit Post: '+ post.title,
+		    allAuthors,
+		    post,
+		    oldData: req.body.editorjs,
+		    errors: errors.array()
+		});
+	    }
+	    else
+	    { // Valid
+		// Doing again as $addToSet can only be used with
+		// query, not as changing value of member array
+		const updatedPost= await Post.findByIdAndUpdate(
+		    req.params.postId,
+		    {
+			title: req.body.title,
+			author: author._id,
+			content: req.body.editorjs,
+			$addToSet: { editorAdmins: [ req.session.userid ] }
+		    }
+		);
+		
+		res.redirect(updatedPost.url);
+	    }
+	}
+	else
+	    res.sendStatus(404);
+    })
+];
--- routes/admins.js
+++ routes/admins.js
@@ -1,7 +1,16 @@
+// Contains all routes related to administration including access
+// management and management of posts
+
 const express= require('express');
 const router= express.Router();
 
 const adminController= require('../controllers/adminController');
+const dashboardController= require('../controllers/dashboardController');
+const editorjsImageHandler= require('../modules/editorjs-image-handler')('public/images');
+const editorjsParseStringToObject = require('../modules/editorjs-parse-string-to-object');
+
+// access management routes
+
 
 router.get('/login',[ adminController.alreadyLoggedIn,
 		      adminController.login_get ]);
@@ -15,11 +24,30 @@ router.use('/*', adminController.loginRedirect);
 
 // all routes below are authenticated
 
-router.get('/dashboard', adminController.dashboard);
-
 router.get('/logout', adminController.logout);
 
 router.get('/changePassword', adminController.change_password_get);
 router.post('/changePassword', adminController.change_password_post);
+
+
+
+// management routes
+
+router.get('/dashboard', dashboardController.dashboard);
+
+router.use('/', editorjsImageHandler);  // for handling editorjs image uploads
+router.get('/createPost', dashboardController.createPost);
+router.post('/createPost',editorjsParseStringToObject,
+	                  dashboardController.createPostHandle );
+
+// The image upload handler for create post will not work as it sends
+// to the path at which form is hosted/editorJsImageHandler
+// The edit URL has extra component which makes it send to editPost
+router.use('/editPost', editorjsImageHandler);  
+router.get('/editPost/:postId([a-f0-9]{24})', dashboardController.editPost);
+router.post('/editPost/:postId([a-f0-9]{24})',editorjsParseStringToObject,
+ 	                                      dashboardController.editPostHandle );
+
+
 
 module.exports=router;
